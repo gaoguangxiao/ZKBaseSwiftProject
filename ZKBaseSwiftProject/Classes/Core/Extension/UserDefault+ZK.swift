60801cfa02a133b7d49be7ac94c5af5e687981e5
gaoguangxiao <gaoguangxiao125@sina.com>
2024-01-03 18:23:21 +0800

    修改UserDefault扩展-下沉扩展工具

--- ZKBaseSwiftProject/Classes/Core/Extension/UserDefault+ZK.swift
+++ ZKBaseSwiftProject/Classes/Core/Extension/UserDefault+ZK.swift
@@ -7,33 +7,11 @@
 //
 
 import Foundation
+import GGXSwiftExtension
 
-//定义属性包装器
-@propertyWrapper
-public struct UserDefaultWrapper<T> {
-    private let key: String
-    private let defaultValue: T
-    
-    public init(key: String, defaultValue: T) {
-        self.key = key
-        self.defaultValue = defaultValue
-    }
-    
-    public var wrappedValue: T {
-        get {
-            let value = UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
-            return value
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: key)
-            UserDefaults.standard.synchronize()
-        }
-    }
-}
-
-public enum Keys {
-    public static let token = "token"
-    public static let agreePrivacyPolicy = "agreePrivacyPolicy"
+public extension Keys {
+    static let token = "token"
+    static let agreePrivacyPolicy = "agreePrivacyPolicy"
 }
 
 public extension UserDefaults {
