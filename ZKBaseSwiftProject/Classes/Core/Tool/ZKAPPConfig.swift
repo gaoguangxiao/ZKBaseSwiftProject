ad4579e32fa8831c18daa8a6679754c034e76b75
gaoguangxiao <gaoguangxiao125@sina.com>
2024-04-03 14:53:08 +0800

    增加开发测试枚举

--- ZKBaseSwiftProject/Classes/Core/Tool/ZKAPPConfig.swift
+++ ZKBaseSwiftProject/Classes/Core/Tool/ZKAPPConfig.swift
@@ -11,6 +11,14 @@ class ZKAPPConfig {
     
 }
 
+
+public enum DistributeChannel {
+    case dev  //开发
+    case test //测试
+    case alpha   //
+    case online  //上线：服务器环境为正式，开启审核
+}
+
 public enum DevelopmentMode {
     //调试模式
     public static var isTest: Bool {
