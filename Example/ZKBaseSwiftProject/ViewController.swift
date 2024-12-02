410a20969067e76b58d56ee34761bc8999ed273d
gaoguangxiao <gaoguangxiao125@sina.com>
2024-12-02 09:48:36 +0800

    feat：tbl列表父类

--- Example/ZKBaseSwiftProject/ViewController.swift
+++ Example/ZKBaseSwiftProject/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import ZKBaseSwiftProject
+import SwiftUI
 
 class ViewController: UIViewController {
 
@@ -22,3 +24,35 @@ class ViewController: UIViewController {
 
 }
 
+struct UpdateSpinView: View {
+    
+    var body: some View {
+        
+        Button {
+            
+        } label: {
+            Text("窗户")
+        }
+
+    }
+}
+
+//使用实例
+class GXRecordExcelViewController: ZKBaseHostingVc<UpdateSpinView> {
+    
+    public init(excels: Array<UpdateSpinView>) {
+        let view = UpdateSpinView()
+        super.init(rootView: view)
+        self.modalPresentationStyle = .fullScreen
+    }
+    
+    @MainActor required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func backBtnClick() {
+        print("返回")
+//        self.dis()
+        self.dismiss(animated: false)
+    }
+}
--- ZKBaseSwiftProject.podspec
+++ ZKBaseSwiftProject.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
     s.name             = 'ZKBaseSwiftProject'
-    s.version          = '1.0.9'
-    s.summary          = 'feat：伙伴之家测试按钮'
+    s.version          = '1.1.0'
+    s.summary          = 'feat：tbl列表父类'
     
     # This description is used to generate tags and improve search results.
     #   * Think: What does it do? Why did you write it? What is the focus?
